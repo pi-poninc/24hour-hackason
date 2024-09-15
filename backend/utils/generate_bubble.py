fcea378ddc7c9d9597bce01d8f78049720d54398
Yoshi-Egawa <seti.yoshizo@gmail.com>
2024-09-15 12:12:54 +0900

    hoge

--- backend/utils/generate_bubble.py
+++ backend/utils/generate_bubble.py
@@ -4,29 +4,35 @@ import io
 import base64
 from PIL import Image, ImageDraw, ImageFont
 
+
 def create_talk_scipt(comments):
     talks = re.findall(r"「(.*?)」", comments)
     speakers = re.findall(r"【(.*?)】", comments)
     if len(talks) != len(speakers):
-        speakers += ["司会者"for i  in range(len(talks) - len(speakers))]
+        speakers += ["司会者" for i in range(len(talks) - len(speakers))]
     return talks, speakers
-def _write_buble(talk_script:list, speakers, width=512, max_chars_per_line=15, size=25) -> list:
+
+
+def _write_buble(
+    talk_script: list, speakers, width=512, max_chars_per_line=15, size=25
+) -> list:
     dicts = []
-    x = width - 25 
+    x = width - 25
     num_script = len(talk_script)
     for i in range(num_script):
-        if i == round(num_script/2):        
+        if i == round(num_script / 2):
             x = 100 * i
         else:
-            col = len(talk_script[i])//max_chars_per_line + 1
-            x -= size 
-            x -= col * size + 20 
+            col = len(talk_script[i]) // max_chars_per_line + 1
+            x -= size
+            x -= col * size + 20
         if speakers[i] == "司会者":
-            y = 40
+            y = 20
         else:
-            y = 80
+            y = 40
         dicts.append({"text": talk_script[i], "xy": (x, y), "speaker": speakers[i]})
     return dicts
+
 
 def _split_text(text, max_chars_per_line):
     """
@@ -35,18 +41,22 @@ def _split_text(text, max_chars_per_line):
     :param max_chars_per_line: 1行あたりの最大文字数
     :return: 改行されたテキストリスト
     """
-    return [text[i:i+max_chars_per_line] for i in range(0, len(text), max_chars_per_line)]
+    return [
+        text[i : i + max_chars_per_line]
+        for i in range(0, len(text), max_chars_per_line)
+    ]
+
 
 def draw_speech_bubble_with_vertical_text(
-        image, 
-        xy, 
-        text, 
-        font,
-        speaker,
-        color=(0, 0, 0), 
-        bubble_color=(255, 255, 255),
-        max_chars_per_line=5
-        ):
+    image,
+    xy,
+    text,
+    font,
+    speaker,
+    color=(0, 0, 0),
+    bubble_color=(255, 255, 255),
+    max_chars_per_line=5,
+):
     """
     吹き出し付きの縦書きテキストを画像に描画し、指定文字数ごとに改行する関数
     :param image: Pillowで開いた画像
@@ -62,9 +72,9 @@ def draw_speech_bubble_with_vertical_text(
 
     # テキストを指定された文字数で分割
     lines = _split_text(text, max_chars_per_line)
-    if len(text) >  max_chars_per_line:
+    if len(text) > max_chars_per_line:
         total_heights = len(text)
-    else: 
+    else:
         total_heights = max_chars_per_line
     # 吹き出しのサイズを計算（テキスト全体の高さと幅）
     max_width = 0
@@ -82,13 +92,26 @@ def draw_speech_bubble_with_vertical_text(
 
     # 吹き出し（楕円形）の描画
     if speaker == "司会者":
-        draw.rectangle([bubble_left, bubble_top, bubble_right, bubble_bottom], fill=(245,245,255), outline=(0, 0, 0), width=2)
+        draw.rectangle(
+            [bubble_left, bubble_top, bubble_right, bubble_bottom],
+            fill=(245, 245, 255),
+            outline=(0, 0, 0),
+            width=2,
+        )
     else:
-        draw.rectangle([bubble_left, bubble_top, bubble_right, bubble_bottom], fill=(245,245,240), outline=(0, 0, 0), width=2)
-
+        draw.rectangle(
+            [bubble_left, bubble_top, bubble_right, bubble_bottom],
+            fill=(245, 245, 240),
+            outline=(0, 0, 0),
+            width=2,
+        )
 
     # 吹き出しの尾（三角形）の描画
-    triangle = [(bubble_right - 30, bubble_bottom), (bubble_right - 10, bubble_bottom), (bubble_right - 20, bubble_bottom + 30)]
+    triangle = [
+        (bubble_right - 30, bubble_bottom),
+        (bubble_right - 10, bubble_bottom),
+        (bubble_right - 20, bubble_bottom + 30),
+    ]
     draw.polygon(triangle, fill=bubble_color, outline=(0, 0, 0))
 
     # 縦書きテキストを描画
@@ -98,8 +121,9 @@ def draw_speech_bubble_with_vertical_text(
         bbox = font.getbbox(line, direction="ttb")
         y = y_defult
         draw.text((x, y), line, font=font, fill=color, direction="ttb")
-        x -= bbox[2] # 行間のスペースを追加
+        x -= bbox[2]  # 行間のスペースを追加
     return image
+
 
 def expand_image(image: Image, target_ratio=16 / 9):
     width, height = image.size
@@ -113,31 +137,39 @@ def expand_image(image: Image, target_ratio=16 / 9):
     new_img.paste(image, (paste_x, 0))
     return new_img
 
-    
+
 def generate_page(base64_img, scene_content, show=False):
     # 画像を開く
     imgdata = base64.b64decode(str(base64_img))
     image = expand_image(Image.open(io.BytesIO(imgdata)))
     page_speech_bubble, speakers = create_talk_scipt(scene_content)
-    size=20
+    size = 20
     # フォントを指定（.ttfファイルが必要、サイズも指定）
-    font = ImageFont.truetype("GenEiKoburiMin6-R.ttf",size=size, layout_engine=ImageFont.Layout.RAQM)
+    font = ImageFont.truetype(
+        "GenEiKoburiMin6-R.ttf", size=size, layout_engine=ImageFont.Layout.RAQM
+    )
     # テキストの内容、位置、色を指定
     # 画像にテキストを描画
-    bubles = _write_buble(page_speech_bubble, speakers,max_chars_per_line=20, width=image.size[0], size=size)
-    
+    bubles = _write_buble(
+        page_speech_bubble,
+        speakers,
+        max_chars_per_line=20,
+        width=image.size[0],
+        size=size,
+    )
+
     for buble in bubles:
         # draw.text(**buble, direction="ttb", font=font)
-        draw_speech_bubble_with_vertical_text(image, **buble, font=font, color=(0, 0, 0), max_chars_per_line=20)
+        draw_speech_bubble_with_vertical_text(
+            image, **buble, font=font, color=(0, 0, 0), max_chars_per_line=20
+        )
     if show:
         return image.show()
     # PILイメージをバイトストリームに変換
     buffered = io.BytesIO()
     image.save(buffered, format="PNG")  # PNGフォーマットで保存
-    
+
     # バイトストリームをbase64エンコードされた文字列に変換
     img_str = base64.b64encode(buffered.getvalue()).decode()
-    
+
     return img_str
-
-
